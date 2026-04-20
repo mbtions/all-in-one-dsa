532e3051c1f216a5c41c66099f11fa33a9d01f35
mbtions <workbymeenakshi@gmail.com>
2026-04-20 22:36:53 +0530

    added Brute force and optimized approach

--- L4_MEDIAN_OF_TWO_SORTED_ARRAYS.java
+++ L4_MEDIAN_OF_TWO_SORTED_ARRAYS.java
@@ -16,12 +16,74 @@ class L4_MEDIAN_OF_TWO_SORTED_ARRAYS {
         sc.close();
 
         // brute force approach
-        System.out.println(findMedianSortedArrays(nums1, nums2) + "");
+        System.out.println(findMedianSortedArraysBFA(nums1, nums2) + "");
+
+        // optimized approach
+        System.out.println(findMedianSortedArraysOPT(nums1, nums2) + "");
     }
 
-    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    public static double findMedianSortedArraysBFA(int[] nums1, int[] nums2) {
         double median = 0;
+        int[] nums3 = new int[nums1.length + nums2.length];
+        int i = 0, j = 0, k = 0;
 
+        while (k != (nums1.length + nums2.length)) {
+            if (i != nums1.length && j != nums2.length) {
+                if (nums1[i] < nums2[j])
+                    nums3[k] = nums1[i++];
+                else
+                    nums3[k] = nums2[j++];
+            } else if (i != nums1.length) {
+                nums3[k] = nums1[i++];
+            } else if (j != nums2.length) {
+                nums3[k] = nums2[j++];
+            }
+            System.out.print(nums3[k] + " ");
+            k++;
+        }
+
+        if (k % 2 == 0) // numbers, n is even
+            median = (double) (nums3[k / 2 - 1] + nums3[k / 2]) / 2.0;
+        else
+            median = nums3[k / 2]; // n is odd
+        return median;
+    }
+
+    public static double findMedianSortedArraysOPT(int[] nums1, int[] nums2) {
+        double median = 0;
+        if (nums1.length == 0 && nums2.length == 1)
+            return nums2[0];
+        if (nums1.length == 1 && nums2.length == 0)
+            return nums1[0];
+        if (nums1.length == 0 && nums2.length == 0)
+            return 0;
+
+        int[] nums3 = new int[nums1.length + nums2.length];
+        int i = 0, j = 0, k = 0;
+        int p = nums1.length + nums2.length;
+
+        while (k != p) {
+            if (i != nums1.length && j != nums2.length) {
+                if (nums1[i] < nums2[j])
+                    nums3[k] = nums1[i++];
+                else
+                    nums3[k] = nums2[j++];
+            } else if (i != nums1.length) {
+                nums3[k] = nums1[i++];
+            } else if (j != nums2.length) {
+                nums3[k] = nums2[j++];
+            }
+            if (k == p / 2) {
+                if (p % 2 != 0) {
+                    median = nums3[k];
+                    break;
+                } else {
+                    median = (double) ((nums3[k] + nums3[k - 1]) / 2.0);
+                    break;
+                }
+            }
+            k++;
+        }
         return median;
     }
 }
