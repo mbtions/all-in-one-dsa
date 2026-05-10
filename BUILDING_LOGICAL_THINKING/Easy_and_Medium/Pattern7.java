bf79bcedafa32567ead87660e0a15f3035638846
mbtions <workbymeenakshi@gmail.com>
2026-05-10 16:55:44 +0530

    added docstring

--- BUILDING_LOGICAL_THINKING/Easy_and_Medium/Pattern7.java
+++ BUILDING_LOGICAL_THINKING/Easy_and_Medium/Pattern7.java
@@ -2,6 +2,27 @@ package BUILDING_LOGICAL_THINKING.Easy_and_Medium;
 
 import java.util.*;
 
+/**
+ * To print a star triangle pattern in the following way:
+ * 
+ * Input: n = 5
+ * Ouptut:
+ * 
+ * ____*
+ * ___***
+ * __*****
+ * _*******
+ * *********
+ * 
+ * underscore represents space.
+ * 
+ * n-1 spaces (1+0 or 2*1-1) stars
+ * n-2 spaces (2+1 or 2*2-1) stars
+ * n-3 spaces (3+2 or 2*3-1) stars
+ * n-4 spaces (4+3 or 2*4-1) stars
+ * n-5 spaces (5+4 or 2*5-1) stars
+ */
+
 class Pattern7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
---
+++ BUILDING_LOGICAL_THINKING/Easy_and_Medium/Pattern9.java
@@ -0,0 +1,49 @@
+package BUILDING_LOGICAL_THINKING.Easy_and_Medium;
+
+import java.util.Scanner;
+
+public class Pattern9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        pattern_diamond(n);
+    }
+
+    public static void pattern_diamond(int n) {
+
+        for (int i = 0; i < n * 2; i++) {
+            int j = 1;
+            if (i < 5) {
+                // traingle
+
+                // spaces
+                for (; j < n - i; j++) {
+                    System.out.print(" ");
+                }
+
+                // stars triangle pattern
+                for (; j <= n + i; j++) {
+                    System.out.print("*");
+                }
+
+            } else {
+                // inverted triangle
+
+                // spaces
+                for (; j < i - n + 1; j++) {
+                    System.out.print(" ");
+                }
+
+                // stars inverted triangle pattern
+                for (; j < n + (n - (i - n)); j++) {
+                    System.out.print("*");
+                }
+            }
+
+            System.out.println();
+
+        }
+    }
+}
