affff45b14e2a1d241b83146aee8a4ac1c76032d
mbtions <workbymeenakshi@gmail.com>
2026-05-12 23:21:45 +0530

    Added Solution to Problem 16

--- L16_3SUM_CLOSEST.java
+++ L16_3SUM_CLOSEST.java
@@ -1,5 +1,49 @@
+import java.util.*;
+
 public class L16_3SUM_CLOSEST {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] nums = new int[size];
+        for (int i = 0; i < size; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        sc.close();
 
+        System.out.println(threeSumClosest(nums, target));
+    }
+
+    public static int threeSumClosest(int[] nums, int target) {
+
+        // sort the array elements
+        Arrays.sort(nums);
+
+        int closestSum = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1; // left pointer
+            int right = nums.length - 1; // right pointer
+
+            while (left < right) {
+                // current sum
+                int sum = nums[i] + nums[left] + nums[right];
+
+                // closest sum is updated
+                if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+                    closestSum = sum;
+                }
+
+                // Matches exactly
+                if (sum == target) {
+                    return target;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return closestSum;
     }
 }
